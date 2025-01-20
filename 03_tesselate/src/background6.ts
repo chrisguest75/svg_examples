d2f5368e67d47ff86696b58e69e214218fb4b7c0
Chris Guest <chrisguest75@users.noreply.github.com>
2025-01-20 19:41:12 +0000

    feat(tesselation): :sparkles: Add a lighthouse tesselation

--- 03_tesselate/src/background6.ts
+++ 03_tesselate/src/background6.ts
@@ -40,11 +40,11 @@ function drawPolygon(centre: Point,  points: Array<Point>, color: string) {
 
 function drawRadial(center: Point, base: number, colour: number, startAngle: number) {
 
-  const outerPoints2 = polygon(base + 200, 6, startAngle);
-  const outerPoints1 = polygon(base + 100, 12, startAngle);
-  const innerPoints = polygon(base, 6, startAngle);
+  const outerPoints2 = polygon(base + 0, 8, startAngle);
+  const outerPoints1 = polygon(base + 50, 8, startAngle);
 
-  
+  drawPolygon(center, outerPoints1, 'hsl('+ colour +',50%,50%)');
+
   let o1 = outerPoints1.length - 1;
   for (let o2 = 0; o2 < outerPoints2.length; o2++) {
     let newPoints = [];
@@ -52,39 +52,43 @@ function drawRadial(center: Point, base: number, colour: number, startAngle: num
     newPoints.push(outerPoints1[o1]);
     o1 = (o1 + 1) % outerPoints1.length;
     newPoints.push(outerPoints1[o1]);
-    drawPolygon(center, newPoints, 'hsl('+ colour +',70%,30%)');
+    drawPolygon(center, newPoints, 'hsl('+ colour + 50 +',70%,40%)');
 
-    newPoints[0] = innerPoints[o2];
+    newPoints = [];
+    newPoints.push(outerPoints2[o2]);
+    o2 = (o2 + 1) % outerPoints2.length;
+    newPoints.push(outerPoints1[o2]);
+    newPoints.push(outerPoints1[o1]);
+    o1 = (o1 + 1) % outerPoints1.length;
     drawPolygon(center, newPoints, 'hsl('+ colour +',70%,30%)');
 
     newPoints = [];
     newPoints.push(outerPoints2[o2]);
+    o2 = (o2 + 1) % outerPoints2.length;
+    newPoints.push(outerPoints1[o2]);
     newPoints.push(outerPoints1[o1]);
     o1 = (o1 + 1) % outerPoints1.length;
-    newPoints.push(outerPoints1[o1]);
-    drawPolygon(center, newPoints, 'hsl('+ colour  +',70%,30%)');
-
-    newPoints[0] = innerPoints[o2];
-    drawPolygon(center, newPoints, 'hsl('+ colour  +',70%,30%)');
+    drawPolygon(center, newPoints, 'hsl('+ colour +',70%,50%)');
 
   }
 }
 
-export function background2(dimensions: Dimensions, baseColour: number, baseAngle: number = 0) {
+export function background6(dimensions: Dimensions, baseColour: number, baseAngle: number = 0) {
   const center = {x: dimensions.width / 2, y: dimensions.height / 2}
-  drawRadial(center, 0, 10 + baseColour, baseAngle);
-  drawRadial(center, 100, 20 + baseColour, baseAngle + (Math.PI / 6));
-  drawRadial(center, 200, 30 + baseColour, baseAngle);
-  drawRadial(center, 300, 40 + baseColour, baseAngle + (Math.PI / 6));
+  drawRadial(center, 1500, 60 + baseColour, baseAngle);
+  drawRadial(center, 1400, 60 + baseColour, baseAngle);
+  drawRadial(center, 1300, 60 + baseColour, baseAngle);
+  drawRadial(center, 1200, 60 + baseColour, baseAngle);
+  drawRadial(center, 1100, 60 + baseColour, baseAngle);
+  drawRadial(center, 1000, 60 + baseColour, baseAngle);
+  drawRadial(center, 900, 60 + baseColour, baseAngle);
+  drawRadial(center, 800, 60 + baseColour, baseAngle);
+  drawRadial(center, 700, 60 + baseColour, baseAngle);
+  drawRadial(center, 600, 60 + baseColour, baseAngle);
+  drawRadial(center, 500, 60 + baseColour, baseAngle);
   drawRadial(center, 400, 50 + baseColour, baseAngle);
-  drawRadial(center, 500, 60 + baseColour, baseAngle + (Math.PI / 6));
-  drawRadial(center, 600, 70 + baseColour, baseAngle);
-  drawRadial(center, 700, 80 + baseColour, baseAngle + (Math.PI / 6));
-  drawRadial(center, 800, 90 + baseColour, baseAngle);
-  drawRadial(center, 900, 100 + baseColour, baseAngle + (Math.PI / 6));
-  drawRadial(center, 1000, 110 + baseColour, baseAngle);
-  drawRadial(center, 1100, 120 + baseColour, baseAngle + (Math.PI / 6));
-  drawRadial(center, 1200, 130 + baseColour, baseAngle);
-  drawRadial(center, 1300, 140 + baseColour, baseAngle + (Math.PI / 6));
-  drawRadial(center, 1400, 150 + baseColour, baseAngle);
+  drawRadial(center, 300, 40 + baseColour, baseAngle);
+  drawRadial(center, 200, 30 + baseColour, baseAngle);
+  drawRadial(center, 100, 20 + baseColour, baseAngle);
+  drawRadial(center, 0, 10 + baseColour, baseAngle);
 }
--- 03_tesselate/src/main.ts
+++ 03_tesselate/src/main.ts
@@ -4,6 +4,7 @@ import { background2 } from './background2';
 import { background3 } from './background3';
 import { background4 } from './background4';
 import { background5 } from './background5';
+import { background6 } from './background6';
 
 function resize() { 
   const width = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
@@ -41,9 +42,13 @@ function redraw() {
       background5({ width, height });
       break;
   
+    case 'Background6':
+      background6({ width, height }, Math.random() * 360, Math.random() * Math.PI);
+      break;
+
     default:
       // wip 
-      background5({ width, height });
+      background6({ width, height }, Math.random() * 360, Math.random() * Math.PI);
       break;
   }
 }
@@ -51,12 +56,13 @@ function redraw() {
 function initialise() {
   redraw();
   addEventListener("resize", (event) => redraw());
-  d3.select('#algorithm').append('option').attr('value', 'Background1').text('Background1');
-  d3.select('#algorithm').append('option').attr('value', 'Background2').text('Background2');
+  d3.select('#algorithm').append('option').attr('value', 'Background1').text('Scales');
+  d3.select('#algorithm').append('option').attr('value', 'Background2').text('Portal');
   d3.select('#algorithm').append('option').attr('value', 'Background3').text('Background3');
-  d3.select('#algorithm').append('option').attr('value', 'Background4').text('Background4');
-  d3.select('#algorithm').append('option').attr('value', 'Background5').text('Background5');
-  d3.select('#algorithm').node().value = 'Background3';
+  d3.select('#algorithm').append('option').attr('value', 'Background4').text('Jelly Tots Mosaic');
+  d3.select('#algorithm').append('option').attr('value', 'Background5').text('Triangle');
+  d3.select('#algorithm').append('option').attr('value', 'Background6').text('Lighthouse');
+  d3.select('#algorithm').node().value = 'Background6';
 
   d3.select('#algorithm').on('change', () => redraw());
 }
